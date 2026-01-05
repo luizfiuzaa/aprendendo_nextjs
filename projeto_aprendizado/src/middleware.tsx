84e48c2140bbabb42cdf5470209fb60813cf1ee2
luizfiuzaa <luizffreitas2006@gmail.com>
2026-01-05 18:33:09 -0300

    :rocket: middleware

--- projeto_aprendizado/src/middleware.tsx
+++ projeto_aprendizado/src/middleware.tsx
@@ -1,10 +1,10 @@
-import {  NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware() {
+export function middleware(request: NextRequest) {
   const autheticated = false;
 
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-  
+  if (request.nextUrl.pathname.startsWith("/dashboard") && !autheticated) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
   return NextResponse.next();
 }
